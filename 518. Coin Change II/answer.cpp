5f76a485b4b1491fb09fd72226cffc512be56b25
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-01-27 23:02:54 +0530

    Create answer.cpp

---
+++ 518. Coin Change II/answer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int> (amount+1));
+      //initialization--------------------------------
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<amount+1; j++){
+                if(i == 0)
+                    dp[i][j] = 0;
+                if(j == 0)
+                    dp[i][j] = 1;    
+            }
+        }
+       //code for choice diagram----------------------------
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<amount+1; j++){
+                if(coins[i-1] <= j)
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];   
+            }
+        }
+        return dp[n][amount];
+        
+    }
+};
