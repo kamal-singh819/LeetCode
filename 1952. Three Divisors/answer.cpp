f6ea496f1184850f46cc9d65162cb1bfe04e1d37
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-01 22:28:38 +0530

    Create answer.cpp

---
+++ 1952. Three Divisors/answer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int count = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i == 0){
+                count++;
+            }
+            if(count > 3){
+                return false;
+            }
+        }
+        if(count == 3)
+            return true;
+        return false;    
+        
+    }
+};
