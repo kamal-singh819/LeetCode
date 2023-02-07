d3f3b465e8a01be8d344fb09db33cb3f502e641c
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-07 19:19:17 +0530

    Update answer.cpp

--- POTD_FEB23/feb7_2023/answer.cpp
+++ POTD_FEB23/feb7_2023/answer.cpp
@@ -22,3 +22,37 @@ public:
         return ans;    
     }
 };
+
+/////////////////////////////////////////////////////////////////////My own////////////////////////////
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int, int> mp;
+        int ans = 0;
+        int i=0;
+        int j=0;
+        while(j < n){
+            mp[fruits[j]]++;
+            // if(mp.size() < 2)
+            //     j++;
+            if(mp.size() <= 2){
+                ans = max(ans, j-i+1);
+                j++;
+            }
+            else{
+                while(mp.size() > 2){
+                    if(mp[fruits[i]] == 1)
+                        mp.erase(fruits[i]);
+                    else
+                        mp[fruits[i]]--;
+                    
+                    i++;
+                }
+                j++;
+            }
+        }
+        return ans;        
+    }
+};
