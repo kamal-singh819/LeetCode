6bf08e82edde1006ba697ec3e364fd2a9ddbbe6b
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-09 15:24:57 +0530

    Update answer.cpp

--- 215. Kth Largest Element in an Array/answer.cpp
+++ 215. Kth Largest Element in an Array/answer.cpp
@@ -1,3 +1,33 @@
+
+//   USING MIN HEAP   //////////
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        //use min heap cozz we have to find largest element....
+        priority_queue<int, vector<int>, greater<int> > minHeap;
+        for(int x : nums){
+            minHeap.push(x);
+            if(minHeap.size() > k)
+                minHeap.pop();
+        }
+
+        return minHeap.top();
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
