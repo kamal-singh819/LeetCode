a3cc9355bdae1cd1128a196ae1f6f7a8800c2286
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-14 02:30:51 +0530

    Update code.cpp

--- 581. Shortest Unsorted Continuous Subarray/code.cpp
+++ 581. Shortest Unsorted Continuous Subarray/code.cpp
@@ -27,3 +27,32 @@ public:
         
     }
 };
+
+////////////////////////////////////////////   Time Complexity is O(N) and S.C is (1)..............
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n = nums.size();
+        //first find end--
+        int mx = nums[0];
+        int end = -1;
+        for(int i=1; i<n; i++){
+            if(mx > nums[i])
+                end = i;
+            else
+                mx = nums[i];
+        }
+
+        //now find start--
+        int mn = nums[n-1];
+        int start = 0;
+        for(int i=n-2; i>=0; i--){
+            if(mn < nums[i])
+                start = i;
+            else
+                mn = nums[i];
+        }
+
+        return end-start+1;  
+    }
+};
