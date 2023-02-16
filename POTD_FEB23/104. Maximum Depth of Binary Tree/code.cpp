e37d6d7a8658f5eef7b2b3cc7cb866e56a0235dc
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-16 23:06:44 +0530

    Create code.cpp

---
+++ POTD_FEB23/104. Maximum Depth of Binary Tree/code.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+
+        int leftTree = maxDepth(root->left);
+        int rightTree = maxDepth(root->right);
+
+        int ans = max(leftTree, rightTree) + 1;
+
+        return ans;
+        
+    }
+};
