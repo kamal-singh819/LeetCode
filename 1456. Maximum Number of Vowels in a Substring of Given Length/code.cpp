3eec7977949301962822431f631c3d38ac53a248
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-18 00:39:26 +0530

    Update code.cpp

--- 1456. Maximum Number of Vowels in a Substring of Given Length/code.cpp
+++ 1456. Maximum Number of Vowels in a Substring of Given Length/code.cpp
@@ -3,6 +3,44 @@ public:
     int maxVowels(string s, int k) {
         if(k > s.length())
             return 0;
+        int i=0,j=0;
+        int n = s.length();
+        int cnt = 0;
+        int ans = INT_MIN;
+        while(j < n){
+            if(j-i+1 < k){
+                if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
+                    cnt++;
+                
+                j++;
+            }
+            else if(j-i+1 == k){
+                if(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
+                    cnt++;
+                ans = max(ans, cnt);
+                j++;
+
+            }
+            else{
+                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                    cnt--;
+                
+                i++;
+            }
+        }
+        return ans;
+        
+    }
+};
+
+
+/////////////////////////////////////////////  USE hashMap   //////////////////////////////////////
+
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        if(k > s.length())
+            return 0;
         unordered_set<char> st;
         st.insert('a');
         st.insert('e');
