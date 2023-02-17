c4a95fcc6d80ee1826eadf0c7a8960f864949a0c
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-02-17 15:52:48 +0530

    Create feb17Code.cpp

---
+++ POTD_FEB23/feb17_2023/feb17Code.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        stack<TreeNode*> st;
+        int ans = INT_MAX;
+        int high;
+        int low = -1000001;
+        int diff = 0;
+        while(root != NULL || !st.empty()){
+            while(root != NULL){
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            high = root->val;
+            diff = high - low;
+            low = high;
+            ans = min(ans, diff);
+            root = root->right;
+        }
+        return ans;
+        
+    }
+};
