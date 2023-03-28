6d2533bf092fd8398bf91533ddbb2d50b2d032d4
Kamal Singh <86668634+kamal-singh819@users.noreply.github.com>
2023-03-28 18:30:57 +0530

    Create imp.cpp

---
+++ 939. Minimum Area Rectangle/imp.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minAreaRect(vector<vector<int>>& points) {
+        int n = points.size();
+        int ans = INT_MAX;
+        unordered_map<int, unordered_set<int>> hash;
+        for(auto x : points){
+            hash[x[0]].insert(x[1]);
+        }
+
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(points[i][1] == points[j][1]){
+                    int x = points[i][0];
+                    int y = points[j][0];
+                    for(auto it : hash[x]){
+                        if(it != points[i][1] && hash[y].find(it) != hash[j].end()){
+                            int area = abs(it-points[i][1]) * abs(points[i][0]-points[j][0]);
+                            ans = min(ans, area);
+                        }
+                    }
+                }
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;   
+    }
+};
